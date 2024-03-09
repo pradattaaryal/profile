229fcc97942d32e16ee6ac74bf72ce14ec074cbf
pradattaaryal <128461960+pradattaaryal@users.noreply.github.com>
2024-03-09 20:18:25 +0545

    Update Landing.jsx

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -9,7 +9,7 @@ import x from './x.png';
         <div className='h-full w-full relative'>
           <img src={x} className='absolute inset-0 w-full h-full object-cover ' alt="Background" />
           <div className=' text-transparent absolute inset-0 flex flex-col  items-center justify-center'>
-             <h1 className={`  font-fira font-extrabold   text-white text-[100px] sm:text-[100px] md:text-[10rem]  xl:text-[200px]  p-2 pb-20 -z-10 `}>初学者</h1>
+             <h1 className={`  font-fira font-extrabold   text-white text-[80px] sm:text-[80px] md:text-[10rem]  xl:text-[200px]  p-2 pb-20 -z-10 `}>初学者</h1>
             <h1 className={`font-fira font-extrabold text-[50px] sm:text-[50px] md:text-[10rem] xl:text-[200px] text-white p-2 -z-10`}> 网页开发者</h1>
           </div>
         </div>
