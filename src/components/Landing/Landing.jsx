c6de709cfc5fd625326cb28429e81f9ad296196d
pradattaaryal <128461960+pradattaaryal@users.noreply.github.com>
2024-03-21 13:49:49 +0545

    Update Landing.jsx

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,17 +1,36 @@
-import React, { useState, useEffect } from 'react'; // Importing useState and useEffect
-import './Landing.css'
+import React, { useState, useEffect } from 'react';
+import './Landing.css';
 import x from './x.png';
-import d from './d.png';
- const Landing = () => {
-   
+import f from './f.png';
+import g from './g.png';
+
+const Landing = () => {
+  const [currentImage, setCurrentImage] = useState(g);
+
+  useEffect(() => {
+    const timer1 = setTimeout(() => {
+      setCurrentImage(f);
+    }, 1000);
+
+    const timer2 = setTimeout(() => {
+      setCurrentImage(x);
+    }, 4000);
+
+    // Clear timeouts on component unmount
+    return () => {
+      clearTimeout(timer1);
+      clearTimeout(timer2);
+    };
+  }, []); // Empty dependency array to run the effect only once
+
   return (
-    <div className='h-screen w-full relative'id='home'> 
-      <div className='max-w-[1140px] m-auto h-screen pt-16 flex justify-center items-center'> 
+    <div className='h-screen w-full relative' id='home'>
+      <div className='max-w-[1140px] m-auto h-screen pt-16 flex justify-center items-center'>
         <div className='h-full w-full relative'>
-          <img src={x} className='absolute inset-0 w-full h-full object-cover ' alt="Background" />
-          <div className=' text-transparent absolute inset-0 flex flex-col  items-center justify-center'>
-             <h1 className={`  font-fira font-extrabold   text-white text-[80px] sm:text-[80px] md:text-[10rem]  xl:text-[200px]  p-2 pb-20 -z-10 `}>初学者</h1>
-            <h1 className={`font-fira font-extrabold text-[60px] sm:text-[60px] md:text-[10rem] xl:text-[200px] text-white p-2 -z-10`}> 网页开发者</h1>
+          <img src={currentImage} className='absolute inset-0 w-full h-full object-cover' alt="Background" />
+          <div className='text-transparent absolute inset-0 flex flex-col items-center justify-center'>
+            <h1 className={`font-fira font-extrabold text-white text-[80px] sm:text-[100px] md:text-[10rem] xl:text-[200px] p-2 pb-20 -z-10`}>初学者</h1>
+            <h1 className={`font-fira font-extrabold text-[50px] sm:text-[80px] md:text-[10rem] xl:text-[200px] text-white p-2 -z-10`}>网页开发者</h1>
           </div>
         </div>
       </div>
