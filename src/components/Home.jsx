c6f296397c0d446e5d1de0d7ce77ae03f978a1fb
pradattaaryal <128461960+pradattaaryal@users.noreply.github.com>
2024-04-19 20:26:03 +0545

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,13 +6,15 @@ import Skill from './Skill/Skill'
 import Portfolio from '../components/Portfolio/Portfolio'
  import Label from './Marquee/Marquee'
 import Footer from './Footer/Footer'
+import Info from './Infopage/Info'
+
  const Home = () => {
   return (
     <>
      <Header></Header>
      <Landing></Landing>
      <About></About>
-     
+     <Info></Info>
       <Skill></Skill>
      <Portfolio></Portfolio>
      <Footer></Footer>
