b4f7ab7d7209f82bcf73918761968106e397be41
pradattaaryal <128461960+pradattaaryal@users.noreply.github.com>
2024-04-25 19:01:38 +0545

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,14 @@ import React from 'react';
  import { BrowserRouter , Route, Routes } from "react-router-dom";
 import Home from './components/Home';
 import About from './components/About/About';
-import Projectsocial from './components/Project';
-
+ 
  const App = () => {
   return (
     <BrowserRouter>
        
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/p" element={<Projectsocial />} />
-
-         <Route path="/about" element={<About />} /> {/* Ensure lowercase 'about' */}
+          <Route path="/about" element={<About />} /> {/* Ensure lowercase 'about' */}
         <Route path="*" element={<div>404 Not Found</div>} />
       </Routes>
     </BrowserRouter>
