c8fc94dec9770671fec46cd6fcc840601a07ff8d
pradattaaryal <128461960+pradattaaryal@users.noreply.github.com>
2024-04-25 19:49:49 +0545

    Update P.jsx

--- src/components/P.jsx
+++ src/components/P.jsx
@@ -3,7 +3,7 @@ import t from './t.png';
 
 export default function P() {
   return (
-    <section className="w-full py-12 md:py-24 lg:py-32 flex justify-center bg-white ">
+    <section className="w-full h-screen py-12 md:py-24 lg:py-32 flex justify-center bg-white ">
 <div className="max-w-[1140px] w-full  border-2 border-black shadow-2xl rounded-2xl p-10">  
  <div className="container    grid items-start gap-6   px-4 md:px-6 lg:grid-cols-2 lg:gap-12">
         <div className="space-y-4">
