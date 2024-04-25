1aab8137a0fa53e70dd0da6fce1c2e57d25e77fb
pradattaaryal <128461960+pradattaaryal@users.noreply.github.com>
2024-04-25 19:43:09 +0545

    Update P.jsx

--- src/components/P.jsx
+++ src/components/P.jsx
@@ -4,7 +4,7 @@ import t from './t.png';
 export default function P() {
   return (
     <section className="w-full py-12 md:py-24 lg:py-32 flex justify-center border-2 border-black shadow-2xl bg-white ">
-      <div className="container grid items-start gap-6   px-4 md:px-6 lg:grid-cols-2 lg:gap-12">
+      <div className="container max-w-[1140px] w-full grid items-start gap-6   px-4 md:px-6 lg:grid-cols-2 lg:gap-12">
         <div className="space-y-4">
           <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">Project Overview</h2>
           <p className="max-w-[600px]  md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed 0">
