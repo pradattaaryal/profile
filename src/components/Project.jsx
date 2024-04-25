ce310f0eb340c8609f2390cfdc9f13821cdac432
pradattaaryal <128461960+pradattaaryal@users.noreply.github.com>
2024-04-25 16:32:08 +0545

    Update Project.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Projectsocial() {
   return (
-    <section className="w-screen py-12 md:py-24 lg:py-32 bg-gray-100 dark:bg-gray-800">
+    <section className="w-screen h-screen py-12 md:py-24 lg:py-32 bg-white dark:bg-gray-800">
       <div className="container grid items-start gap-6 px-4 md:px-6 lg:grid-cols-2 lg:gap-12">
         <div className="space-y-4">
           <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">Project Overview</h2>
