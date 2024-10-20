0002166f524287625c3f91f78cb89498997c05d2
pradattaaryal <128461960+pradattaaryal@users.noreply.github.com>
2024-10-20 18:44:15 +0545

    Update data.js

--- src/components/Portfolio/data.js
+++ src/components/Portfolio/data.js
@@ -4,57 +4,54 @@ import ecommerce from '../../Public/Images/e.png'
  import login from '../../Public/Images/login.png'
 import bf from '../../Public/Images/bf.png'
 import t from '../../Public/Images/t.png'
+import c from '../../Public/Images/c.jpeg'
 
 
-const data=[   
- {
+const data = [
+    {
         id: "1",
+        src: c,  
+        title: "Food Delivery App",
+        cat: "Fullstack",
+        link: "https://food-delivery-frontend-gilt.vercel.app/"
+    },
+    {
+        id: "2",
         src: t,
         title: "Mern-Socialedia",
-        cat:"Fullstack",
-         link:"https://profile-eight-dun.vercel.app/p"
-
-    },   
- {
-    id: "2",
-    src: ecommerce,
-    title: "ecommerce",
-    cat:"full",
-    link:"https://eco-5s21.vercel.app/",
-link2:"https://eco-backend-boxa.onrender.com/"
+        cat: "Fullstack",
+        link: "https://profile-eight-dun.vercel.app/p"
     },
     {
         id: "3",
-        src: Portfolio,
-        title: "Portfolio",
-        cat:"front",
-         link:"https://profile-eight-dun.vercel.app/" 
-        
+        src: ecommerce,
+        title: "Ecommerce",
+        cat: "Fullstack",
+        link: "https://eco-5s21.vercel.app/",
+        link2: "https://eco-backend-boxa.onrender.com/"
     },
     {
         id: "4",
-        src: login,
-        title: "Authentication:refresh, accress,google",
-        cat:"front",
-         link:"https://auth-beta-teal.vercel.app/"
-
+        src: Portfolio,
+        title: "Portfolio",
+        cat: "Frontend",
+        link: "https://profile-eight-dun.vercel.app/"
     },
     {
         id: "5",
-        src: bf,
-        title: "react-frontend",
-        cat:"front",
-         link:"https://beach-frontend.vercel.app/"
-
+        src: login,
+        title: "Authentication: Refresh, Access, Google",
+        cat: "Frontend",
+        link: "https://auth-beta-teal.vercel.app/"
     },
-     {
+    {
         id: "6",
-        src: xx,
-        title: "simple java chapapp",
-        cat:"full",
-         link:"https://github.com/pradattaaryal/java-simple-chatapplicatio/tree/main"
+        src: bf,
+        title: "React Frontend",
+        cat: "Frontend",
+        link: "https://beach-frontend.vercel.app/"
+    }
+];
 
-    },
-    
-]
 export default data;
+ 
