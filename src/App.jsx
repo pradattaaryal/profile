ee3ec856625ff2ca241015d60d5f3deaf9ddfd9c
pradattaaryal <pradattaaryal2468@gmail.com>
2025-03-28 19:51:47 +0545

    csdcs

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import P from './components/P';
 const App = () => {
   return (
     <Router>
-       
+      { /*sdcldslcnsdkjn*/ }
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/P" element={<P/>} />
