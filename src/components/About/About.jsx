24d6d2c271631d46342dc85bcccd56555983aaa4
pradattaaryal <pradattaaryal2468@gmail.com>
2025-08-03 09:27:10 +0545

    smaller imageld

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import y from "./image.jpeg";
+import y from "./image.jpg";
 import CV from "./CV.pdf";
 import { IoDocumentTextOutline } from "react-icons/io5";
 import { MdWorkOutline, MdOutlineMailOutline } from "react-icons/md";
