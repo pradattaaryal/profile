7c44dc9dc9fa009a004c405339ec37ba73328722
pradattaaryal <pradattaaryal2468@gmail.com>
2025-08-15 21:28:03 +0545

    xsxas

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,12 +6,8 @@ import { FaFacebook, FaInstagram, FaTwitter,FaGithub,FaLinkedin   } from 'react-
    return (
     <div className='max-h-[45vh] border-t-2 mt-44 md:mt-0 border-black border-solid w-full bg-white  ' id='contact'>
     <div className='max-w-[1140px] h-full    m-auto flex flex-col    items-center'>
-      <h1 className=' p-3 sm:p-5 md:p-10   text-lg sm:text-xl  md:text-3xl  xl:text-5xl tracking-wide font-lg  '>Pradatta Aryal</h1>
-      <ul className='gap-8 flex pb-3 sm:pb-5 md:pb-10   text-lg sm:text-xl  md:text-1xl font-mono xl:text-2xl tracking-wide font-lg'>
-      <li>About</li>
-      <li>Project</li>
-      <li>Skill</li>
-      </ul>
+      <h1 className=' p-3 sm:p-5 md:p-10 font-bold   text-lg sm:text-xl  md:text-3xl  xl:text-5xl tracking-wide font-lg  '>Pradatta Aryal</h1>
+      
       <div className='flex  gap-4 text-lg sm:text-xl  md:text-3xl pb-10'>
          <a href="https://www.facebook.com/pradatta.aryal.94">  <FaFacebook /></a>
          <a href="https://www.instagram.com/pradatta_aryal/"> <FaInstagram /></a>
--- src/components/Skill/Skill.jsx
+++ src/components/Skill/Skill.jsx
@@ -17,18 +17,22 @@ const Skill = () => {
   };
 
   return (
-    <div className="w-full  h-screen max-w-[1140px]  m-auto  flex items-center p-4 justify-center flex-col" id='skill'>
-              <div className='md:h-[25vh] sm:h-[25vh] md:pt-12 sm:pt-8 flex flex-col items-center'>
-          <h1 className='p-2 pt-4 text-xl sm:text-3xl md:text-5xl xl:text-7xl tracking-wide font-semibold'>Skills</h1>
-          <p className='text-xl mb-4 font-medium'>My technical level</p>
-        </div>
-      <div className="max-w-[1140px] w-full lg:w-auto text-xs md:text-base border-2 border-black rounded-xl p-6 lg:px-28 shadow-2xl m-4 lg:m-auto">
-        
-        <pre className="rounded-md overflow-hidden">
+ <div
+      className="w-full h-screen max-w-[1140px] m-auto flex flex-col items-center justify-center  p-4"
+      id="skill"
+    >
+      <div className="  flex flex-col items-center text-center">
+        <h2 className="text-4xl md:text-5xl font-bold">Skills</h2>
+        <p className="text-xl font-medium">My technical level</p>
+      </div>
+
+      <div className="max-w-[1140px]   w-full lg:w-auto text-xs md:text-base border-2 border-black rounded-xl p-6 lg:px-28 shadow-2xl mt-4">
+        <pre className="rounded-md font-semibold overflow-hidden">
           {JSON.stringify(studentData, null, 2)}
         </pre>
       </div>
     </div>
+
   );
 };
 
